f22c7d169d94788fdf6e7342fa00c40315445c9b
Anton <onepix2000@gmail.com>
2020-01-27 09:46:50 +0100

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Test on 27/01/2020.
+//  Copyright © 2020 Peanch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
